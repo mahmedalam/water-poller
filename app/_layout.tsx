16439a4e4cbb11008ffe196536345777cb95dd9b
Ahmed Alam <mahmedalamdev@gmail.com>
2025-12-25 02:47:48 +0500

    feat(fonts): add Inter font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-4xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+      <Text className="text-4xl text-center text-blue-500 font-inter-bold">
+        Welcome to my React Native Application!
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
